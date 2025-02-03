d7a7936e82a9606ec69ab43d39cb9f55090e3187
eirikhs <eirik.skjaerseth@gmail.com>
2025-02-03 14:38:25 +0100

    sett opp repo (#1)
    Co-authored-by: Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>

---
+++ src/test/kotlin/no/nav/helse/flex/FellesTestOppsett.kt
@@ -0,0 +1,24 @@
+package no.nav.helse.flex
+
+import no.nav.security.mock.oauth2.MockOAuth2Server
+import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
+import org.junit.jupiter.api.TestInstance
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcPrint
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@AutoConfigureObservability
+@EnableMockOAuth2Server
+@SpringBootTest(classes = [Application::class, MockWebServereConfig::class])
+@AutoConfigureMockMvc(print = MockMvcPrint.NONE, printOnlyOnFailure = false)
+abstract class FellesTestOppsett {
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Autowired
+    lateinit var server: MockOAuth2Server
+}
