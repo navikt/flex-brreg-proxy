7ca3ed967fb2268e282f87d9df941a386240398f
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2025-02-03 14:28:13 +0100

    format

--- src/test/kotlin/no/nav/helse/flex/ApiTest.kt
+++ src/test/kotlin/no/nav/helse/flex/ApiTest.kt
@@ -7,7 +7,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 class ApiTest : FellesTestOppsett() {
-
     @Test
     fun `burde ikke ha tilgang til brreg`() {
         val result =
@@ -15,7 +14,7 @@ class ApiTest : FellesTestOppsett() {
                 .perform(
                     MockMvcRequestBuilders
                         .get("/api/v1/check-brreg-auth")
-                        .contentType(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON),
                 ).andExpect(MockMvcResultMatchers.status().isOk)
                 .andReturn()
                 .response.contentAsString
