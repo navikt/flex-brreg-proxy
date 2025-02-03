6440c9f28d9c5cc03f1545648f953d32f61645e1
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2025-02-03 17:23:54 +0100

    kaller på brreg med secrets

---
+++ src/main/kotlin/no/nav/helse/flex/BrregScheduledCaller.kt
@@ -0,0 +1,38 @@
+package no.nav.helse.flex
+
+import generated.rolleutskrift.Grunndata
+import no.nav.helse.flex.config.logger
+import no.nav.helse.flex.config.serialisertTilString
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
+
+@Component
+class BrregScheduledCaller(
+    private val rolleutskriftClient: RolleutskriftClient,
+) {
+    private val log = logger()
+
+    @Scheduled(initialDelay = 1, fixedDelay = 30, timeUnit = TimeUnit.SECONDS)
+    fun callBrreg() {
+        val res = rolleutskriftClient.hentRolleutskriftRaw("04817797240")
+        log.info(res?.responseHeader?.toHeaderString())
+        try {
+            log.info(res?.serialisertTilString())
+            val meldingJson = res?.melding?.serialisertTilString()
+            log.info(meldingJson)
+        } catch (e: Exception) {
+            log.error("Failed to serialize response", e)
+        }
+    }
+}
+
+private fun Grunndata.ResponseHeader.toHeaderString(): String {
+    val understatuser = this.underStatus?.underStatusMelding?.joinToString(", ") { "${it.kode}: ${it.value}" }
+    return "ResponseHeader(" +
+        "requestId=$requestID, " +
+        "hovedStatus=$hovedStatus, " +
+        "underStatusMeldinger=$understatuser, " +
+        "orgnr=${this.orgnr}, " +
+        "tjeneste=${this.tjeneste})"
+}
